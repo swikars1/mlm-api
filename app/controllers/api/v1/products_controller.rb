4ecafff14c46d9791a1687a4d56c9fd803e6075d
Swikar <swikarsharma@gmail.com>
2020-10-09 11:31:33 +0545

    product retailer filter added

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::ProductsController < ApplicationController
   def index
     products = Product.all
-    products = Product.where('name ilike ?', "%#{params[:q]}%") unless params[:q].empty?
+    products = products.where('name ilike ?', "%#{params[:q]}%") unless params[:q]&.empty?
+    products = products.where(retailer_id: params[:retailer_id]) if params[:retailer_id]
     render json: { data: products }, status: :ok
   end
 end
