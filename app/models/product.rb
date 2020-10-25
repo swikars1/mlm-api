f927c491e0dbffa367d5c3dcbfa240638a9c7429
sunilregmi233 <sunilregmi223@gmail.com>
2020-10-25 04:04:11 +0000

    Dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :customer_products, dependent: :destroy
   has_many :customers, through: :customer_products
   has_many :payments
-
+  has_many :category_products
+  has_many :categories, through: :category_products
   has_one_attached :avatar
 end
