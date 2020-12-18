9b652ae0a6cd13b05cf58a21af6a509932140d0d
sunilregmi233 <sunilregmi223@gmail.com>
2020-12-18 16:10:29 +0545

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:restart'
+      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
   end
