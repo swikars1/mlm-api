86f87713454d8566ef066c83b2e5f8baa0bff96f
sunilregmi233 <sunilregmi223@gmail.com>
2020-12-29 13:25:08 +0545

    add email to retailer and database conncetions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_162918) do
+ActiveRecord::Schema.define(version: 2020_12_29_072712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 2020_12_26_162918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "percent"
+    t.string "email"
     t.index ["name"], name: "index_retailers_on_name"
   end
 
