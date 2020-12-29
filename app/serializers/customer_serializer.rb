838c2a78a594d46f2bc2ce738e8565d8493d04ad
Swikar <swikarsharma@gmail.com>
2020-12-30 03:23:43 +0545

    payment show

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,6 +1,6 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone_no, :gender, :address, :birthday, :expenditure, :refer_code, :image_url,
-             :bills, :front_url, :back_url, :status
+             :bills, :front_url, :back_url, :status, :todays_income, :monthly_income
 
   def image_url
   	object.image_url(object.user.avatar)
@@ -18,6 +18,14 @@ class CustomerSerializer < ActiveModel::Serializer
 
   def back_url
     object.image_url(object.id_back)
+  end
+
+  def todays_income
+    object.profits.where('created_at between ? and ?', Time.zone.now.beginning_of_day, Time.zone.now).sum(:self_profit).round(2)
+  end
+
+  def monthly_income
+    object.profits.where('created_at between ? and ?', Time.zone.now - 1.month, Time.zone.now).sum(:self_profit).round(2)
   end
 
   def status
--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -1,5 +1,6 @@
 class PaymentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :expenditure, :customer, :retailer, :percent, :cashback
+  attributes :id, :name, :expenditure, :customer, :retailer, :percent, :cashback,
+             :self_profit, :company_profit, :each_parent_profit, :profit_parent_count
 
   def customer
   	object.customer.name
@@ -10,10 +11,26 @@ class PaymentSerializer < ActiveModel::Serializer
   end
 
   def percent
-    object.retailer&.percent
+    object.retailer&.percent || 0
   end
 
   def cashback
-    object.expenditure * (object.retailer&.percent.to_f / 100)
+    (object.expenditure * (object.retailer&.percent.to_f / 100)).to_f.round(2)
+  end
+
+  def self_profit
+    object.profits.where(customer_id: object.customer_id).sum(:self_profit).to_f.round(2) || 0
+  end
+
+  def company_profit
+    object.profits.where(customer_id: object.customer_id).sum(:company_profit).to_f.round(2) || 0
+  end
+
+  def each_parent_profit
+    object.profits.where.not(customer_id: object.customer_id).sample&.self_profit&.to_f&.round(2) || 0
+  end
+
+  def profit_parent_count
+    object.profits.where.not(customer_id: object.customer_id)&.count || 0
   end
 end
